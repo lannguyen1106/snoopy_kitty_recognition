56136e246a03d49d6d4181efd229874e09471cc2
jodythai <thaibduong@gmail.com>
2019-08-23 16:13:31 +0700

    update

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 import numpy as np
+import os
 import tensorflow as tf
-from flask import Flask, escape, render_template, request
+from flask import Flask, escape, render_template, request, send_from_directory
 
 app = Flask(__name__)
+
+def parse_image(imgData):
+    imgstr = re.search(b"base64,(.*)", imgData).group(1)
+    img_decode = base64.decodebytes(imgstr)
+    with open("output.jpg", "wb") as file:
+        file.write(img_decode)
+    return img_decode
 
 @app.route("/")
 def main():
      return render_template("home.html")
+
+@app.route("/upload/", methods=["POST"])
+def upload_file():
+    img_raw = parse_image(request.get_data())
+
+    # image = tf.image.decode_jpeg(img_raw, channels=1)
+    # image = tf.image.resize(image, [28, 28])
+    # image = (255 - image) / 255.0  # normalize to [0,1] range
+    # image = tf.reshape(image, (1, 28, 28, 1))
+
+    # probabilites = model.predict(image)
+    # prediction = np.argmax(probabilites, axis=1)
+    # return str(prediction)
+
+    return img_raw
---
+++ static/js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  console.log("Hello Kitty!")
+
+  $('#fileupload').fileupload({
+    url: '/upload/',
+    type: 'POST'
+  });
+
+  /*
+  function handle_upload(e) {
+    console.log('Upload File Stating!');
+    // console.log(SCRIPT_ROOT)
+
+    var formData = new FormData();
+    var img = $('#upload')[0].files[0];
+
+    formData.append("file", img)
+
+    console.log(img)
+
+    try {
+      $.ajax({
+        type: "POST",
+        url: "/upload/",
+        data: formData,
+        success: function(data){
+          console.log('Success')
+          $('#result').html('<img src=' + data + ' alt="" />');
+        }
+      });
+    } catch (error) {
+      console.log(error)
+    } finally {}
+  }
+
+  $('form').on('submit', handle_upload);
+
+  // $('#button_upload').click(handle_upload);
+  */
+
+});
