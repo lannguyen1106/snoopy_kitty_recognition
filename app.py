4511f0c2810d310d206270aeb54746ee91bd6928
jodythai <thaibduong@gmail.com>
2019-08-23 14:23:43 +0700

    update

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ from flask import Flask, escape, render_template, request
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
-
-
 @app.route("/")
-def hello():
-    name = request.args.get("name", "Duong")
-    return f"Hello, {escape(name)}!"
+def main():
+     return render_template("home.html")
