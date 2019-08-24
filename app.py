18a0cab85f10386f9cb2f1c70745fdd05ff92f70
jodythai <thaibduong@gmail.com>
2019-08-24 22:08:42 +0700

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import logging
 import re
 import tensorflow as tf
-from flask import Flask, escape, render_template, request, send_from_directory, redirect
+from flask import Flask, flash, escape, render_template, request, send_from_directory, redirect, session, url_for
 from flask_dropzone import Dropzone
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from flask_debugtoolbar import DebugToolbarExtension
@@ -13,66 +13,109 @@ app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'Jody Thai'
 
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
-dropzone = Dropzone(app)
+# dropzone = Dropzone(app)
 
 # Dropzone settings
-app.config['DROPZONE_UPLOAD_MULTIPLE'] = False
-app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
-app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
-app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
+# app.config['DROPZONE_UPLOAD_MULTIPLE'] = False
+# app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+# app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+# app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
 
-app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+# app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
 
-photos = UploadSet('photos', IMAGES)
-configure_uploads(app, photos)
-patch_request_class(app)
+# photos = UploadSet('photos', IMAGES)
+# configure_uploads(app, photos)
+# patch_request_class(app)
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 model = tf.keras.models.load_model("static/models/my_model.h5")
 
 def parse_image(imgData):
-  logging.info(imgData)
   imgstr = re.search(b"base64,(.*)", imgData).group(1)
   img_decode = base64.decodebytes(imgstr)
   with open("output.jpg", "wb") as file:
       file.write(img_decode)
   return img_decode
 
-@app.route('/', methods=['GET', 'POST'])
+def allowed_file(filename):
+  return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
 def main():
-  # list to hold our uploaded image urls
-  file_urls = []
-  
-  if request.method == 'POST':
-    file_obj = request.files
-    # for f in file_obj:
-    file = request.files.get(file_obj)
+  # # set session for image results
+  # if "file_urls" not in session:
+  #     session['file_urls'] = []
+  # # list to hold our uploaded image urls
+  # file_urls = session['file_urls']
 
-    img_raw = parse_image(request.get_data())
+  # if request.method == 'POST':
+  #   file_obj = request.files[0]
+
+  #   # for f in file_obj:
+  #   file = request.files.get(file_obj)
     
-    # save the file with to our photos folder
-    # filename = photos.save(
-    #   file,
-    #   name=file.filename    
-    # )               
-    # append image urls
-    # file_urls.append(photos.url(filename))
+  #   # save the file with to our photos folder
+  #   filename = photos.save(
+  #       file,
+  #       name=file.filename
+  #   )
 
-    # Predict
-    image = tf.image.decode_jpeg(img_raw, channels=1)
-    image = tf.image.resize(image, [28, 28])
-    image = (255 - image) / 255.0  # normalize to [0,1] range
-    image = tf.reshape(image, (1, 28, 28, 1))
+  #   # append image urls
+  #   file_urls.append(photos.url(filename))
 
-    probabilites = model.predict(image)
-    prediction = np.argmax(probabilites, axis=1)
+  #   image = file.save(image)
 
-    return str(prediction)
-        
-    return "uploading..."
+  # Predict
+  # image = tf.image.decode_jpeg(image, channels=1)
+  # image = tf.image.resize(image, [28, 28])
+  # image = (255 - image) / 255.0 # normalize to [0,1] range
+  # image = tf.reshape(image, (1, 28, 28, 1))
+
+  # probabilites = model.predict(image)
+  # prediction = np.argmax(probabilites, axis=1)
+
+  # return str(prediction)
 
   return render_template('home.html')
+
+@app.route('/upload-image/', methods=['GET', 'POST'])
+def upload_image():
+  prediction = ''
+
+  if request.method == 'POST':
+
+    if 'file' not in request.files:
+      flash('No file part')
+      return redirect(request.url)
+      
+    file = request.files['file']
+
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+      flash('No selected file')
+      return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      # return redirect(url_for('uploaded_file', filename=filename))
+
+      # img_raw = parse_image(request.get_data())
+
+      image = tf.image.decode_jpeg(file, channels=1)
+      image = tf.image.resize(image, [28, 28])
+      image = (255 - image) / 255.0  # normalize to [0,1] range
+      image = tf.reshape(image, (1, 28, 28, 1))
+
+      probabilites = model.predict(image)
+      prediction = np.argmax(probabilites, axis=1)
+  
+  return str(prediction)
 
 if __name__ == '__main__':
   app.run()
--- static/js/main.js
+++ static/js/main.js
@@ -1,38 +1,21 @@
 $(document).ready(function() {
-  console.log("Hello Kitty!")
 
-  $("#fileupload").dropzone({ url: "/file/post" });
+  
+});
 
-  /*
-  function handle_upload(e) {
-    console.log('Upload File Stating!');
-    // console.log(SCRIPT_ROOT)
+$(document).on('submit', '#form-upload-image', function(e) {
+  e.preventDefault();
 
-    var formData = new FormData();
-    var img = $('#upload')[0].files[0];
+  var form_data = new FormData($('#form-upload-image')[0]);
 
-    formData.append("file", img)
-
-    console.log(img)
-
-    try {
-      $.ajax({
-        type: "POST",
-        url: "/upload/",
-        data: formData,
-        success: function(data){
-          console.log('Success')
-          $('#result').html('<img src=' + data + ' alt="" />');
-        }
-      });
-    } catch (error) {
-      console.log(error)
-    } finally {}
-  }
-
-  $('form').on('submit', handle_upload);
-
-  // $('#button_upload').click(handle_upload);
-  */
-
+  $.ajax({
+    type: 'POST',
+    url: '/upload-image/',
+    processData: false,
+    contentType: false,
+    data: form_data,
+    success: function(data) {
+      $('#result').text('Predicted Output: ' + data);
+    }
+  });
 });
