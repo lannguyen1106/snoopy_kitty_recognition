649b6aa7e012f6220775e91ee80183b7302a0ad7
jodythai <thaibduong@gmail.com>
2019-08-24 17:51:08 +0700

    update

--- app.py
+++ app.py
@@ -1,39 +1,78 @@
 import numpy as np
+import base64
 import os
+import logging
 import re
 import tensorflow as tf
-from flask import Flask, escape, render_template, request, send_from_directory
+from flask import Flask, escape, render_template, request, send_from_directory, redirect
+from flask_dropzone import Dropzone
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'Jody Thai'
+
+toolbar = DebugToolbarExtension(app)
+
+dropzone = Dropzone(app)
+
+# Dropzone settings
+app.config['DROPZONE_UPLOAD_MULTIPLE'] = False
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
+
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)
+
+model = tf.keras.models.load_model("static/models/my_model.h5")
 
 def parse_image(imgData):
-    imgstr = re.search(b"base64,(.*)", imgData).group(1)
-    img_decode = base64.decodebytes(imgstr)
-    with open("output.jpg", "wb") as file:
-        file.write(img_decode)
-    return img_decode
+  logging.info(imgData)
+  imgstr = re.search(b"base64,(.*)", imgData).group(1)
+  img_decode = base64.decodebytes(imgstr)
+  with open("output.jpg", "wb") as file:
+      file.write(img_decode)
+  return img_decode
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def main():
-     return render_template("home.html")
+  # list to hold our uploaded image urls
+  file_urls = []
+  
+  if request.method == 'POST':
+    file_obj = request.files
+    # for f in file_obj:
+    file = request.files.get(file_obj)
 
-@app.route("/upload", methods = ['GET', 'POST'])
-def upload_file():
-    # img_raw = parse_image(request.get_data())
+    img_raw = parse_image(request.get_data())
+    
+    # save the file with to our photos folder
+    # filename = photos.save(
+    #   file,
+    #   name=file.filename    
+    # )               
+    # append image urls
+    # file_urls.append(photos.url(filename))
 
-    if request.method == 'POST':
-      uploaded_file = request.files['file']
+    # Predict
+    image = tf.image.decode_jpeg(img_raw, channels=1)
+    image = tf.image.resize(image, [28, 28])
+    image = (255 - image) / 255.0  # normalize to [0,1] range
+    image = tf.reshape(image, (1, 28, 28, 1))
 
-    # image = tf.image.decode_jpeg(img_raw, channels=1)
-    # image = tf.image.resize(image, [28, 28])
-    # image = (255 - image) / 255.0  # normalize to [0,1] range
-    # image = tf.reshape(image, (1, 28, 28, 1))
+    probabilites = model.predict(image)
+    prediction = np.argmax(probabilites, axis=1)
 
-    # probabilites = model.predict(image)
-    # prediction = np.argmax(probabilites, axis=1)
-    # return str(prediction)
+    return str(prediction)
+        
+    return "uploading..."
 
-    return uploaded_file
+  return render_template('home.html')
 
 if __name__ == '__main__':
-  app.run(debug = True)
+  app.run()
