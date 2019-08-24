64b2c6f6d343567597b756d14e2875121d5bdedc
jodythai <thaibduong@gmail.com>
2019-08-25 01:22:36 +0700

    update

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ import sys
 import tensorflow as tf
 from werkzeug import secure_filename
 from flask import Flask, flash, escape, render_template, request, send_from_directory, redirect, session, url_for, jsonify
-from flask_dropzone import Dropzone
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
-from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.debug = True
@@ -17,28 +14,17 @@ app.config['SECRET_KEY'] = 'Jody Thai'
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
+# load the trained model
+model = tf.keras.models.load_model("static/models/my_model_tl_sigmoid_acc95.h5")
+
+# change these two values to match the image width and height in the trained model
 IMAGE_WIDTH = 120
 IMAGE_HEIGHT = 120
 
-# toolbar = DebugToolbarExtension(app)
-
-# dropzone = Dropzone(app)
-
-# Dropzone settings
-# app.config['DROPZONE_UPLOAD_MULTIPLE'] = False
-# app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
-# app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
-# app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
-
-# app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
-
-# photos = UploadSet('photos', IMAGES)
-# configure_uploads(app, photos)
-# patch_request_class(app)
-
-model = tf.keras.models.load_model("static/models/my_model_tl_sigmoid_acc95.h5")
-
 def allowed_file(filename):
+  """
+  Security purpose: this function will check if the uploaded file extension is allowed or not
+  """
   return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
@@ -47,57 +33,54 @@ def main():
 
 @app.route('/upload-image/', methods=['GET', 'POST'])
 def upload_image():
-
+  """
+  This function handle the upload image request from the frond end user interface
+  """
   prediction_probs = ''
   label = ''
   
   if request.method == 'POST':
-
+    
+    # check if the file is not appropriate
     if 'file' not in request.files:
       flash('No file part')
       return redirect(request.url)
 
+    # retrieve the image from the ajax request
     file = request.files['file']
 
     # if user does not select file, browser also
     # submit an empty part without filename
     if file.filename == '':
-      flash('No selected file')
-      return redirect(request.url)
+      return jsonify({'label': 'No file selected', 'probs': ''})
 
     if file and allowed_file(file.filename):
+      # security stuff
       filename = secure_filename(file.filename)
+      # get the full file path
       upload_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+      # save the image to our uploads folder
       file.save(upload_file_path)
-      # return redirect(url_for('uploaded_file', filename=filename))
-
-      # img_raw = parse_image(request.get_data())
+      
+      # preprocess the image for predicting
       image_raw = tf.io.read_file(upload_file_path)
       image = tf.image.decode_jpeg(image_raw, channels=3)
       image = tf.image.resize(image, [IMAGE_HEIGHT, IMAGE_WIDTH])
       image = (255 - image) / 255.0  # normalize to [0,1] range
       image = tf.reshape(image, (1, IMAGE_HEIGHT, IMAGE_WIDTH, 3))
 
+      # predict the image
       probs = model.predict(image)
-      print('probs\n', file=sys.stdout)
-      print(probs, file=sys.stdout)
-      
       prediction = probs[0][0]
-      print('prediction\n', file=sys.stdout)
-      print(prediction, file=sys.stdout)
-      
+
+      # make the label from precision
       label = "Dog" if prediction >= 0.5 else "Cat"
       
+      # change prediction probability to percentage format
       prediction_probs = prediction if prediction >= 0.5 else 1 - prediction
       prediction_probs = round((prediction_probs * 100), 2)
-      print('prediction_probs\n', file=sys.stdout)
-      print(prediction_probs, file=sys.stdout)
-      # label = "Dog" if probs >= 0.5 else "Cat"
-      # prediction = np.argmax(probabilites, axis=1)
-      # prediction_probs = probs if probs >= 0.5 else 1 - probs
-      # prediction_probs = round((prediction_probs[0][0] * 100), 2)
-
-      predict_image = base64.b64encode(image_raw)
+      # print('prediction_probs\n', file=sys.stdout)
+      # print(prediction_probs, file=sys.stdout)
   
   return jsonify({'label': label, 'probs': str(prediction_probs)})
 
